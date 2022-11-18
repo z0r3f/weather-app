1d8b4dbe331a6da84a2b92a79b40bddc4361efbb
Fernando Ocaña <ferozar@gmail.com>
2022-11-18 19:45:47 +0100

    Little change

--- weather-delivery/src/main/kotlin/me/fernando/chat/db/sql/AlertPostgresRepository.kt
+++ weather-delivery/src/main/kotlin/me/fernando/chat/db/sql/AlertPostgresRepository.kt
@@ -4,10 +4,8 @@ import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.CrudRepository
 import me.fernando.chat.db.entities.AlertEntity
-import me.fernando.chat.db.entities.ChatEntity
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 interface AlertPostgresRepository : CrudRepository<AlertEntity, Long> {
-    fun findByChat(chat: ChatEntity): List<AlertEntity>
     fun findByHourOfDay(hourOfDay: Int): List<AlertEntity>
 }
