fade9ccc016019496cb6e4a93efa01489db7da92
Fernando Ocaña <ferozar@gmail.com>
2023-04-01 16:59:08 +0200

    #16 Change selection of displayed temperatures

--- weather-core/src/main/kotlin/me/fernando/util/CustomExtensions.kt
+++ weather-core/src/main/kotlin/me/fernando/util/CustomExtensions.kt
@@ -11,3 +11,14 @@ fun String.trimLeadingSpaces() = this.replace("(?m)(^ +)".toRegex(), "").trim()
 fun OverviewService<Unit>.generateOverviewMessage() = generateOverviewMessage(Unit)
 
 fun Any?.isNull() = this == null
+
+fun <T> List<T>.minElementsBy(comparator: Comparator<in T>): List<T> {
+    val minElement = this.minWithOrNull(comparator)
+    return if (minElement != null) {
+        this.filter { element ->
+            comparator.compare(element, minElement) == 0
+        }
+    } else {
+        emptyList()
+    }
+}
--- weather-core/src/test/kotlin/me/fernando/weather/service/ForecastOverviewServiceTest.kt
+++ weather-core/src/test/kotlin/me/fernando/weather/service/ForecastOverviewServiceTest.kt
@@ -3,6 +3,7 @@ package me.fernando.weather.service
 import me.fernando.weather.domain.ForecastMother
 import me.fernando.weather.domain.WeatherDataMother
 import me.fernando.weather.domain.WeatherMother
+import me.fernando.weather.service.comparator.HourComparator
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import java.time.Instant
@@ -10,7 +11,7 @@ import java.time.ZoneId
 
 internal class ForecastOverviewServiceTest {
 
-    private val sut = ForecastOverviewService()
+    private val sut = ForecastOverviewService(HourComparator())
 
     @Test
     fun generateForecastOverviewMessage() {
@@ -18,9 +19,14 @@ internal class ForecastOverviewServiceTest {
 
         assertAll(
             { assert(actual.contains("*Madrid*")) },
-            { assert(actual.contains("15-02-2022")) },
-            { assert(actual.contains("16-02-2022")) },
-            { assert(!actual.contains("17-02-2022")) },
+            { assert(actual.contains("*dom 02-04-2023*")) },
+            { assert(actual.contains("`🌤 2°C|🌤 14°C|🌤 7°C`")) },
+            { assert(actual.contains("*lun 03-04-2023*")) },
+            { assert(actual.contains("`🌤 2°C|🌤 15°C|🌤 9°C`")) },
+            { assert(actual.contains("*mar 04-04-2023*")) },
+            { assert(actual.contains("`🌤 3°C|🌤 16°C|🌤 7°C`")) },
+			{ assert(actual.contains("*mié 05-04-2023*")) },
+			{ assert(actual.contains("`🌤 1°C|🌤 16°C|🌤 8°C`")) },
         )
     }
 
@@ -29,60 +35,244 @@ internal class ForecastOverviewServiceTest {
             forecasts =
             listOf(
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-15T07:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 12.0,
-                    weather = listOf(WeatherMother().of(icon = "01n"))
+                    timeDataForecasted = Instant.parse("2023-04-01T12:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 14.21,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-15T13:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 23.0,
-                    weather = listOf(WeatherMother().of(icon = "02d"))
+                    timeDataForecasted = Instant.parse("2023-04-01T15:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 15.08,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-15T20:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 18.0,
-                    weather = listOf(WeatherMother().of(icon = "01n"))
+                    timeDataForecasted = Instant.parse("2023-04-01T18:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 13.15,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-16T07:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 13.0,
-                    weather = listOf(WeatherMother().of(icon = "01n"))
+                    timeDataForecasted = Instant.parse("2023-04-01T21:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 9.23,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-16T13:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 24.0,
-                    weather = listOf(WeatherMother().of(icon = "01n"))
+                    timeDataForecasted = Instant.parse("2023-04-02T00:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 5.37,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-16T20:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 17.0,
-                    weather = listOf(WeatherMother().of(icon = "02d"))
+                    timeDataForecasted = Instant.parse("2023-04-02T03:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 3.09,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-17T13:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 24.0,
-                    weather = listOf(WeatherMother().of(icon = "01n"))
+                    timeDataForecasted = Instant.parse("2023-04-02T06:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 2.09,
+                    weather = listOf(WeatherMother().of())
                 ),
                 ForecastMother().of(
-                    timeDataForecasted = Instant.parse("2022-02-17T20:00:00.00Z").atZone(
-                        ZoneId.of("Europe/Madrid")
-                    ).toLocalDateTime(),
-                    temperature = 17.0,
-                    weather = listOf(WeatherMother().of(icon = "02d"))
+                    timeDataForecasted = Instant.parse("2023-04-02T09:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 9.16,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-02T12:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 14.36,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-02T15:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 15.64,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-02T18:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 12.11,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-02T21:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 6.5,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T00:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 3.97,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T03:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 2.54,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T06:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 1.68,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T09:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 9.31,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T12:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 14.77,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T15:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 17.17,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T18:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 13.95,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-03T21:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 9.14,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T00:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 6.28,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T03:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 3.94,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T06:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 2.76,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T09:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 9.68,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T12:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 15.72,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T15:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 17.8,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T18:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 12.84,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-04T21:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 6.55,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T00:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 3.55,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T03:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 2.04,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T06:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 1.26,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T09:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 10.11,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T12:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 16.36,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T15:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 18.41,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T18:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 13.85,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-05T21:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 8.18,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-06T00:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 4.97,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-06T03:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 2.85,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-06T06:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 1.88,
+                    weather = listOf(WeatherMother().of())
+                ),
+                ForecastMother().of(
+                    timeDataForecasted = Instant.parse("2023-04-06T09:00:00.00Z").atZone(ZoneId.of("Europe/Madrid"))
+                        .toLocalDateTime(),
+                    temperature = 9.92,
+                    weather = listOf(WeatherMother().of())
                 ),
             ),
         )
