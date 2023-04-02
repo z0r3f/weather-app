8206bec361dd89b128818530af5ffc5232b564a2
Fernando Ocaña <42670787+z0r3f@users.noreply.github.com>
2023-04-02 07:16:24 +0200

    Remove `UTC` on feedback alert (#21)

--- weather-core/src/main/kotlin/me/fernando/weather/service/RemoveAlertOverviewService.kt
+++ weather-core/src/main/kotlin/me/fernando/weather/service/RemoveAlertOverviewService.kt
@@ -5,6 +5,6 @@ import jakarta.inject.Singleton
 @Singleton
 class RemoveAlertOverviewService: OverviewService<Int> {
     override fun generateOverviewMessage(hourOfDay: Int): String {
-        return "Deleted alert at $hourOfDay:00 UTC every day"
+        return "Deleted alert at $hourOfDay:00 every day"
     }
 }
